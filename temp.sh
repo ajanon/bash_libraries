cd24932241b3d16acc8403f60e4b949dc2ed7dea
Alexis Janon <98706738+ajanon@users.noreply.github.com>
2019-05-14 18:00:39 +0200

    temp.sh: library for creating temporary dirs
    A temporary directory can be created using temp_dir. For now, the
    function exits if it cannot create a temporary dir (this may
    change in the future).
    A cleanup function is registered: all temporary directories will be
    removed on exit of the sourcing script.

---
+++ temp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash    
+
+temp_dir() {
+    local -r dirpath="$(mktemp -d)"
+    if [[ ! "$dirpath" || ! -d "$dirpath" ]]; then
+        if declare -f -F log_critical >/dev/null; then
+            log_critical "Could not create temporary directory. Aborting."
+        else
+            printf "Could not create temporary directory. Aborting.\n"
+        fi
+        exit 1
+    fi
+    if [[ -z ${__TEMP_DIR_LIST__+x} ]]; then
+        declare -ag __TEMP_DIR_LIST__=("$dirpath")
+        trap temp_dir_cleanup EXIT
+    fi
+    echo "${dirpath}"
+}
+
+temp_dir_cleanup() {
+    for dirpath in "${__TEMP_DIR_LIST__[@]}"; do
+        if ! rm -rf "$dirpath"; then
+            if declare -f -F log_critical >/dev/null; then
+                log_critical "Could not delete directory %s. Aborting." "$dirpath"
+            else
+                printf "Could not delete directory %s. Aborting.\n" "$dirpath"
+            fi
+        fi
+    done
+}
